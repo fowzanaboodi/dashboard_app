4cc6a3d2c83285a96bba2b776f06f2a9b2f3338a
fowzanaboodi <basha@sulhuf.com>
2022-04-13 01:27:33 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+  
+    </NavigationContainer>
+  );
+}
+
+export default App;
